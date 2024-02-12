5fd182d671a958b9306f5ec59584bc152c778898
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-02-12 02:38:07 +0000

    Hello World

---
+++ com/example/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * My program Says hello to Jaydin and says have a nice day!.
+ * @author  Kent Gatera
+ * @version 1.0
+ * @since   2024-Feb-8
+ */
+
+/*
+ * My first class
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello Jaydin!");
+    System.out.println("Have a nice day!");
+  }
+}
